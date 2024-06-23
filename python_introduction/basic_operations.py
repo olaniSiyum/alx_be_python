b38449f6d772ebf15bf4d4e4e2fa0ba95c53119c
olaniSiyum <olisiyum@gmail.com>
2024-06-23 08:06:23 -0700

    modified file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,7 @@
 number1 = 10
 number2 = 5
 x = number1 + number2
-y = number2 - number1
+y = number1 - number2
 z = number1 * number2cd
 print(f"Addition of 10 and 5 is {x}")
 print(f"Subtraction of 10 and 5 is {y}")
