2f1a13d55a5563420ac098f1246b7f91499c0f9f
olaniSiyum <olisiyum@gmail.com>
2024-07-21 11:46:39 -0700

    modified book_class.py file

--- oop/book_class.py
+++ oop/book_class.py
@@ -3,16 +3,14 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        print(f"Book instance created: {self}")
-
-    def __del__(self):
-        print(f"Deleting {self.title}")
+        print(f"Book instance created: {self.title}', '{self.author}', {self.year}")
 
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
-
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
+    def __del__(self):
+        print(f"Deleting {self.title}")
 if __name__ == "__main__":
     # Creating an instance of Book
     my_book = Book("1984", "George Orwell", 1949)
